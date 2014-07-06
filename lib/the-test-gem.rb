73b443ff00f3b88fd90697b1e97b90707b3c5728
Sebastian <sebastian.poell@gmail.com>
2014-07-06 17:49:35 +0200

    test

--- lib/the-test-gem.rb
+++ lib/the-test-gem.rb
@@ -1,1 +1,15 @@
 require "the-test-gem/version"
+
+module The
+  module Test
+    module Gem
+    
+       # Use Rails engine
+       module Rails
+          class Engine < ::Rails::Engine
+          end
+       end
+       
+    end
+  end
+end
