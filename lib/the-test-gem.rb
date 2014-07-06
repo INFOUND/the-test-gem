4a315b851cd9fd2c667aa0b1c6fb40564d1a87ef
Sebastian Pöll <sebastian.poell@gmail.com>
2014-07-06 17:11:28 +0200

    Update the-test-gem.rb
    Another test

--- lib/the-test-gem.rb
+++ lib/the-test-gem.rb
@@ -1,14 +1,1 @@
 require "the-test-gem/version"
-
-module The
-  module Test
-    module Gem
-    
-       module Rails
-          class Engine < ::Rails::Engine
-          end
-       end
-
-    end
-  end
-end
